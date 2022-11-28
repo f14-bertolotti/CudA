ce68a6073882e5884e6a1e400b5c84a999ca570f
Francesco Bertolotti <f14.bertolotti@gmail.com>
2022-11-28 20:30:37 +0100

    game of life without wrapping edges

--- src/main.cu
+++ src/main.cu
@@ -20,59 +20,35 @@
 #include <sys/types.h>
 #include <SFML/Graphics.h>
 
-#define BLOCK_SIZE 16
 
+void print_matrix(int size, int* grid) {
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            printf("%d",grid[i * size + j]);
+        }
+        printf("\n");
+    }
+}
 
 __global__ void game_of_life(int size, int* grid, int* new_grid) {
     int ix = blockDim.x * blockIdx.x + threadIdx.x;
     int iy = blockDim.y * blockIdx.y + threadIdx.y;
     int id = iy * size + ix;
     int numNeighbors;
+    
 
-    if (1 < ix && ix < size-1 && 1 < iy && iy < size-1) {
-        numNeighbors = grid[(ix-1) * size + iy+1] + grid[(ix+0) * size + iy+1] + grid[(ix+1) * size + iy+1] + 
-                       grid[(ix-1) * size + iy+0] + grid[(ix+0) * size + iy+0] + grid[(ix+1) * size + iy+0] +
-                       grid[(ix-1) * size + iy-1] + grid[(ix+0) * size + iy-1] + grid[(ix+1) * size + iy-1]; 
-        new_grid[id] = 0 * (new_grid[id] == 1 && numNeighbors < 2) + 
-                       1 * (new_grid[id] == 1 && numNeighbors == 2 || numNeighbors == 3) + 
-                       0 * (new_grid[id] == 1 && numNeighbors > 3) +
-                       1 * (new_grid[id] == 0 && numNeighbors == 3) + 
-                       new_grid[id] * (new_grid[id] == 0 && numNeighbors != 3);
+    if (0 < ix && ix < size-1 && 0 < iy && iy < size-1) {
+        numNeighbors = grid[(iy-1) * size + ix+1] + grid[(iy+0) * size + ix+1] + grid[(iy+1) * size + ix+1] + 
+                       grid[(iy-1) * size + ix+0] + 0                          + grid[(iy+1) * size + ix+0] +
+                       grid[(iy-1) * size + ix-1] + grid[(iy+0) * size + ix-1] + grid[(iy+1) * size + ix-1]; 
+        new_grid[id] = 0        * (grid[id] == 1 && numNeighbors  < 2) + 
+                       1        * (grid[id] == 1 && (numNeighbors == 2 || numNeighbors == 3)) + 
+                       0        * (grid[id] == 1 && numNeighbors  > 3) +
+                       1        * (grid[id] == 0 && numNeighbors == 3) + 
+                       grid[id] * (grid[id] == 0 && numNeighbors != 3);
     }
-}
 
-__global__ void GOL(int dim, int *grid, int *newGrid)
-{
-    // We want id ∈ [1,dim]
-    int iy = blockDim.y * blockIdx.y + threadIdx.y + 1;
-    int ix = blockDim.x * blockIdx.x + threadIdx.x + 1;
-    int id = iy * (dim+2) + ix;
-    
-    int numNeighbors;
-    
-    if (iy < dim-2 && ix < dim-2) {
-    
-        // Get the number of neighbors for a given grid point
-        numNeighbors = grid[id+(dim+2)] + grid[id-(dim+2)] //upper lower
-                    + grid[id+1] + grid[id-1]             //right left
-                    + grid[id+(dim+3)] + grid[id-(dim+3)] //diagonals
-                    + grid[id-(dim+1)] + grid[id+(dim+1)];
-        
-        int cell = grid[id];
-        // Here we have explicitly all of the game rules
-        if (cell == 1 && numNeighbors < 2)
-            newGrid[id] = 0;
-        else if (cell == 1 && (numNeighbors == 2 || numNeighbors == 3))
-            newGrid[id] = 1;
-        else if (cell == 1 && numNeighbors > 3)
-            newGrid[id] = 0;
-        else if (cell == 0 && numNeighbors == 3)
-            newGrid[id] = 1;
-        else
-            newGrid[id] = cell;
-    }
 }
-
 
 int main(int argc, char* argv[]) {
     int size = 1024;
@@ -97,27 +73,31 @@ int main(int argc, char* argv[]) {
     srand(14);
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
-            host_grid[i * size + j] = rand() % 2;
+            if (0 < i && i < size-1 && 0 < j && j < size-1) host_grid[i * size + j] = rand() % 2;
             sfVertex vertex;
             vertex.color = host_grid[i * size + j] ? sfWhite : sfBlack;
             vertex.position = (sfVector2f){(float) i, (float) j};
             sfVertexArray_append(vertex_array, vertex);
         }
     }
-
-
-    cudaMemcpy(cuda_grid, host_grid, bytes, cudaMemcpyHostToDevice);
-
     dim3 threadsPerBlock(8, 8);
     dim3 numBlocks(128,128);
 
-    sfUint8* update = (sfUint8*) malloc(sizeof(sfUint8) * size * size);
-    while (sfRenderWindow_isOpen(window)) {
+    print_matrix(size, host_grid);
+    printf("\n");
+    cudaMemcpy(cuda_grid, host_grid, bytes, cudaMemcpyHostToDevice);
+
+
+    int i = 0; 
+    while (++i < 1000000) {
+        printf("%d\n",i);
         game_of_life<<<numBlocks,threadsPerBlock>>>(size, cuda_grid, cuda_new_grid);
         cuda_tmp_grid = cuda_grid;
         cuda_grid = cuda_new_grid;
         cuda_new_grid = cuda_tmp_grid;       
         cudaMemcpy(host_grid, cuda_grid, bytes, cudaMemcpyDeviceToHost);
+
+        //print_matrix(size, host_grid);
 
 
         for (int i = 0; i < size; ++i) {
